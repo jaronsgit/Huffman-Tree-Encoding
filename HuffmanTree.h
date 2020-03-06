fc130c79df3c4387823120bd00c23fe1090020a5
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-06 15:32:24 +0200

    Initial commit. Created blank header files for HuffmanTree and HuffanNode.

---
+++ HuffmanTree.h
@@ -0,0 +1,25 @@
+#ifndef HUFFMANTREE_H
+#define HUFFMANTREE_H
+
+namespace CHNJAR003
+{
+
+class HuffmanTree
+{
+private:
+public:
+    HuffmanTree(/* args */);
+    ~HuffmanTree();
+};
+
+HuffmanTree::HuffmanTree(/* args */)
+{
+}
+
+HuffmanTree::~HuffmanTree()
+{
+}
+
+} // namespace CHNJAR003
+
+#endif
