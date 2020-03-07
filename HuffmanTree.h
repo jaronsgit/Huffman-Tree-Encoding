75819d6da6e47d6c19d8a1eb59735a20aa2cbf5c
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-07 23:29:40 +0200

    Made buildFrequencyTable() a private method in HuffmanTree.h

--- HuffmanTree.h
+++ HuffmanTree.h
@@ -15,11 +15,12 @@ private:
     std::priority_queue<HuffmanNode> nodeQueue;
     std::unordered_map<char, int> frequencyTable; //Map of the characters and their associated frequencies
 
+    void buildFrequencyTable(std::string inputFileName);
+
 public:
     HuffmanTree();
     ~HuffmanTree();
 
-    void buildFrequencyTable(std::string inputFileName);
     void buildHuffmanTree(std::unordered_map<char, int> ft); //Build the HuffmanTree representation from map of frequencies
     void compressData(std::string inputFileName, std::string outputFileName);
 };
