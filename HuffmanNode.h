e27f5b6a488e32dcf048afbd63ccecfbb6cbd7ea
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-07 17:40:16 +0200

    Added method declarations in HuffmanNode.h yet to be implemented in HuffmanNode.cpp

---
+++ HuffmanNode.h
@@ -0,0 +1,36 @@
+#ifndef HUFFMANNODE_H
+#define HUFFMANNODE_H
+#include <memory>
+namespace CHNJAR003
+{
+
+class HuffmanNode
+{
+private:
+    char character;
+    int frequency;
+    std::shared_ptr<HuffmanNode> leftChild;
+    std::shared_ptr<HuffmanNode> rightChild;
+
+public:
+    HuffmanNode(void);                              //default constructor
+    ~HuffmanNode();                                 //destructor
+    HuffmanNode(const HuffmanNode &rhs);            //copy constructor
+    HuffmanNode(HuffmanNode &&rhs);                 //move consructor
+    HuffmanNode &operator=(const HuffmanNode &rhs); //assignent operator
+    HuffmanNode &operator=(HuffmanNode &&rhs);      //move assignment operator
+
+    void setCharacter(char c);             //setter for character
+    void setFrequency(int f);              //setter for frequency
+    void setLeftChild(HuffmanNode left);   //setter for left child node
+    void setRightChild(HuffmanNode right); //setter for right child node
+
+    char getCharacter(void);                      //getter for character
+    int getFrequency(void);                       //getter for frequency
+    std::shared_ptr<HuffmanNode> getLeftChild();  //getter for left child node
+    std::shared_ptr<HuffmanNode> getRightChild(); //getter for right child node
+};
+
+} // namespace CHNJAR003
+
+#endif
