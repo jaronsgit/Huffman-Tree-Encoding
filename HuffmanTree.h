8e94dcdc22affed754473a97c31dcb0593efe5ca
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-11 14:28:46 +0200

    Added declarations for getFrequencyTable() and getCodeTable() methods in HuffmanTree.h

--- HuffmanTree.h
+++ HuffmanTree.h
@@ -15,15 +15,16 @@ private:
     std::priority_queue<HuffmanNode> nodeQueue;
     std::unordered_map<char, int> frequencyTable;    //Map of the characters and their associated frequencies
     std::unordered_map<char, std::string> codeTable; //Map of the characters and their associated binary codes
-    void buildFrequencyTable(std::string inputFileName);
-
 public:
     HuffmanTree();
     ~HuffmanTree();
-
+    void buildFrequencyTable(std::string inputFileName);
     void buildHuffmanTree(std::unordered_map<char, int> ft);        //Build the HuffmanTree representation from map of frequencies
     void buildCodeTable(HuffmanNode *node, std::string binaryCode); //Recurse down tree and build table of characters and their binary codes
     void compressData(std::string inputFileName, std::string outputFileName);
+
+    std::unordered_map<char, int> getFrequencyTable() const;
+    std::unordered_map<char, std::string> getCodeTable() const;
 };
 
 } // namespace CHNJAR003
